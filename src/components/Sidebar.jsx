1800ca9a5ec78b76a6722b69e85974030c565403
Juan Ramirez <juanfrawn@gmail.com>
2022-09-02 19:43:50 -0400

    :sparkles: [FEAT] Sidebar component & Utils constants

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { categories } from '../utils/constants'
+
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
---
+++ src/utils/constants.js
@@ -0,0 +1,32 @@
+import {FaMusic} from "react-icons/fa";
+import {FaHome} from "react-icons/fa";
+import {FaCode} from "react-icons/fa";
+import {FaBook} from "react-icons/fa";
+import {FaPodcast} from "react-icons/fa";
+import {FaVideo} from "react-icons/fa";
+import {FaGamepad} from "react-icons/fa";
+import {FaFootballBall} from "react-icons/fa";
+import {FaBitcoin} from "react-icons/fa";
+
+
+export const categories = [
+    { name: 'New', icon: <FaHome />, },
+    { name: 'Dua Lipa', icon: <FaMusic />, },
+    { name: 'Coding', icon: <FaCode />, },
+    { name: 'ReactJS', icon: <FaCode />, },
+    { name: 'NextJS', icon: <FaCode />, },
+    { name: 'Music', icon: <FaMusic /> },
+    { name: 'Education', icon: <FaBook />, },
+    { name: 'Podcast', icon: <FaPodcast />, },
+    { name: 'Movie', icon: <FaVideo />, },
+    { name: 'Gaming', icon: <FaGamepad />, },
+    { name: 'Sport', icon: <FaFootballBall />, },
+    { name: 'Crypto', icon: <FaBitcoin />, },
+  ];
+  
+  export const demoThumbnailUrl = '';
+  export const demoChannelUrl = '';
+  export const demoVideoUrl = '';
+  export const demoChannelTitle = ''
+  export const demoVideoTitle = ' ';
+  export const demoProfilePicture = ''
