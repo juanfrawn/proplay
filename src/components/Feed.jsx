7c410bd48f8b66c939ceb0601db0a6188e523054
Juan Ramirez <juanfrawn@gmail.com>
2022-09-03 14:32:20 -0400

    :sparkles: [FEAT] Fetch home videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Sidebar from './Sidebar'
-import Videos from './Sidebar'
+import Videos from './Videos'
+
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState('New')
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory])
 
   return (
     <div className="flex flex-col md:flex-row w-full">
@@ -13,7 +21,7 @@ const Feed = () => {
           setSelectedCategory={setSelectedCategory}
         />
         <div>
-            
+          <Videos videos={videos} />
         </div>
     </div>
   )
