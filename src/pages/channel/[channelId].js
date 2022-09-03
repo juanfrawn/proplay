9b81d4ae59f34c8a65521d32d998afd0f8b35692
Juan Ramirez <juanfrawn@gmail.com>
2022-09-03 18:10:29 -0400

    :sparkles: [FEAT] Fetch & show channel details

--- src/pages/channel/[channelId].js
+++ src/pages/channel/[channelId].js
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router'
+
+import { fetchFromAPI } from '../../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videoDetail, setVideosDetail] = useState([]);
+  const { query: { channelId } } = useRouter()
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${channelId}`)
+      .then((data) => setChannelDetail(data?.items[0]))
+
+    fetchFromAPI(`search?channelId=${channelId}&part=snippet&order=date`)
+      .then((data) => setVideosDetail(data?.items))
+  }, [channelId])
+  console.log(channelDetail)
+
   return (
-    <div>ChannelDetail</div>
+    <div>
+      <h1>{channelDetail?.snippet.title}</h1>
+    </div>
   )
 }
 
