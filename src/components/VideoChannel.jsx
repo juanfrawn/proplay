fb40a5410898bcc92ff66a8b8909a11f154c1cee
Juan Ramirez <juanfrawn@gmail.com>
2022-09-04 14:16:39 -0300

    :lipstick: [STYLE] Channel page styles

--- src/components/VideoChannel.jsx
+++ src/components/VideoChannel.jsx
@@ -1,26 +1,45 @@
-import Image from "next/image"
-import preview from '../../public/img.jpg'
-import profile from '../../public/profile.jpg'
-import { BsThreeDotsVertical } from 'react-icons/bs';
+import Image from "next/image";
+import preview from "../../public/img.jpg";
+import profile from "../../public/profile.jpg";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
-const VideoChannel = () => {
+const VideoChannel = ({ video: { id, snippet } }) => {
   return (
     <div className="w-full sm:w-76 md:w-80 lg:w-120 mb-5 font-quicksand">
-        <div className="object-cover w-full mb-1">
-            <Image src={preview} alt="image" className="rounded-3xl" />
+      <div className="w-full mb-1">
+        <Image src={snippet?.thumbnails?.high?.url}
+              alt={snippet?.title}
+              width={500}
+              height={200} 
+              className="rounded-3xl object-cover" 
+              />
+      </div>
+      <div className="flex mx-1">
+        <div className="max-w-full w-full">
+          <h3 className="font-bold text-md mb-1">
+            {snippet?.title.slice(0, 70) || "Titulo"}
+          </h3>
+          <p className="text-gray-400 text-sm mb-2">
+            {snippet?.channelTitle || "Channel"}
+          </p>
+          <p className="text-gray-400 text-xs">
+            {snippet?.publishedAt.slice(8, 10)}-
+            {snippet?.publishedAt.slice(5, 7)}-
+            {snippet?.publishedAt.slice(0, 4)}
+            <span className="text-yellow-400 mx-2">•</span>
+            {`${snippet?.publishedAt.slice(
+              11,
+              13
+            )}:${snippet?.publishedAt.slice(14, 16)}`}
+            {parseInt(snippet?.publishedAt.slice(11, 13)) > 12 ? " AM" : " PM"}
+          </p>
         </div>
-        <div className="flex mx-1">
-            <div className="max-w-full w-full">
-                <h3 className="font-bold text-md mb-1">Highlight - Raptor Fortnite</h3>
-                <p className="text-gray-400 text-sm mb-2">Fortnite meat</p>
-                <p className="text-gray-400 text-xs">12m views <span className="text-yellow-400 mx-2">•</span> 2 days ago</p>
-            </div>
-            <div className="text-lg my-2 mr-1 text-gray-400">
-                <BsThreeDotsVertical />
-            </div>
+        <div className="text-lg my-2 mr-1 text-gray-400">
+          <BsThreeDotsVertical />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoChannel
+export default VideoChannel;
--- src/pages/channel/[channelId].js
+++ src/pages/channel/[channelId].js
@@ -13,39 +13,42 @@ const ChannelDetail = () => {
   const [videoDetail, setVideosDetail] = useState([]);
   const { query: { channelId } } = useRouter()
 
-  // useEffect(() => {
-  //   fetchFromAPI(`channels?part=snippet&id=${channelId}`)
-  //     .then((data) => setChannelDetail(data?.items[0]))
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${channelId}`)
+      .then((data) => setChannelDetail(data?.items[0]))
 
-  //   fetchFromAPI(`search?channelId=${channelId}&part=snippet&order=date`)
-  //     .then((data) => setVideosDetail(data?.items))
-  // }, [channelId])
+    fetchFromAPI(`search?channelId=${channelId}&part=snippet&order=date`)
+      .then((data) => setVideosDetail(data?.items))
+  }, [channelId])
+  console.log(videoDetail)
 
   return (
     <div className='text-center '>
-      <div className='w-full h-52 bg-gradient-to-r from-cyan-500 to-blue-500'>
+      <div className='mx-auto h-60 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-4xl'>
       </div>
-      <div className='flex justify-center -translate-y-12 nb-0'>
+      <div className='flex justify-center -translate-y-14 nb-0'>
         <Image 
-          src={profile} 
+          src={channelDetail?.snippet?.thumbnails?.height?.url || channelDetail?.snippet?.thumbnails?.medium?.url} 
           alt="Profile" 
-          width={140}
-          height={140}
+          width={150}
+          height={150}
           className='rounded-full'
         />
       </div>
-      <h1 className='text-4xl font-bold -mt-6 mb-2'>Dua Lipa</h1>
-      <p className='mb-10'><span className='font-bold'>2.000.000</span> Followers</p>
-
+      <h1 className='text-4xl font-bold -mt-10 mb-2'>
+        {channelDetail?.snippet?.title}
+      </h1>
+      <p className='mb-10'><span className='font-bold'>{channelDetail?.statistics?.subscriberCount}</span> Followers</p>
+      <p className=' text-lg text-zinc-400 font-bold w-3/4 mx-auto mb-12'>{channelDetail?.snippet?.description.substring(0, 300)}</p>
       <div className='flex flex-wrap justify-center gap-x-4 text-start'>
-        <VideoChannel />
-        <VideoChannel />
-        <VideoChannel />
-        <VideoChannel />
-        <VideoChannel />
-        <VideoChannel />
-        <VideoChannel />
-        <VideoChannel />
+        {videoDetail?.map((video, idx) => {
+          return (
+            <VideoChannel
+              video={video}
+              key={idx}
+            />
+          )
+        })}
       </div>
     </div>
   )
