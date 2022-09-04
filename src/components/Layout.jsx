8c4fbcf05a0a5a57f5593f0e2cd742e09c572ddc
Juan Ramirez <juanfrawn@gmail.com>
2022-09-03 20:50:41 -0400

    :sparkles: [FEAT] Navbar responsive component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,11 @@
 import Nabvar from './Navbar';
+import NavbarBottom from './NavbarBottom';
 
 const Layout = ({ children }) => {
     return (
         <>
             <Nabvar />
+            <NavbarBottom />
             <main>{ children }</main>
         </>
     )
---
+++ src/components/NavbarBottom.jsx
@@ -0,0 +1,20 @@
+import { AiFillHome, AiFillStar } from 'react-icons/ai'
+import { BsFillBellFill, BsFillChatLeftDotsFill } from "react-icons/bs";
+import { BiPlus } from 'react-icons/bi'
+
+
+const NavbarBottom = () => {
+  return (
+    <div className="fixed bottom-0 bg-gradient-to-r from-primary-light to-primary-lighter w-full py-4 md:hidden">
+        <div className='flex justify-around items-center text-xl text-icons'>
+            <AiFillHome />
+            <AiFillStar />
+            <BiPlus className='text-4xl text-secondary border-2 border-secondary rounded-full' />  
+            <BsFillBellFill />
+            <BsFillChatLeftDotsFill />
+        </div>
+    </div>
+  )
+}
+
+export default NavbarBottom
