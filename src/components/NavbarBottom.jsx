dfba450595c19b338ccc3c26b8834abce5113e85
Juan Ramirez <juanfrawn@gmail.com>
2022-09-05 08:39:26 -0300

    :sparkles: [FEAT] Navbar responsive links

--- src/components/NavbarBottom.jsx
+++ src/components/NavbarBottom.jsx
@@ -1,17 +1,33 @@
-import { AiFillHome, AiFillStar } from 'react-icons/ai'
-import { BsFillBellFill, BsFillChatLeftDotsFill } from "react-icons/bs";
+import Link from 'next/link'
+
 import { BiPlus } from 'react-icons/bi'
+import {GoHome} from 'react-icons/go'
+import {GoGlobe} from 'react-icons/go'
+import {GoStar} from 'react-icons/go'
+import {GoThreeBars} from 'react-icons/go'
 
 
 const NavbarBottom = () => {
   return (
     <div className="fixed z-10 bottom-0 bg-gradient-to-r from-primary-light to-primary-lighter w-full py-4 md:hidden">
         <div className='flex justify-around items-center text-xl text-icons'>
-            <AiFillHome />
-            <AiFillStar />
+            <Link href="/">
+              <GoHome />
+            </Link>
+
+            <Link href="/discover">
+              <GoGlobe />
+            </Link>
+
             <BiPlus className='text-4xl text-secondary border-2 border-secondary rounded-full' />  
-            <BsFillBellFill />
-            <BsFillChatLeftDotsFill />
+            
+            <Link href="/favorites">
+              <GoStar />
+            </Link>
+
+            <Link href="/playlist">
+            <GoThreeBars />
+            </Link>
         </div>
     </div>
   )
